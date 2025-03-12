e5a16bfd7ad2181d5689a889b1042db982747e45
ngicks <yknt.bsl@gmail.com>
2025-03-12 23:05:46 +0900

    apply format

--- lua/custom/lazygit-toggle.lua
+++ lua/custom/lazygit-toggle.lua
@@ -1,6 +1,6 @@
-local Terminal  = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 
-local lazygit = Terminal:new({
+local lazygit = Terminal:new {
   cmd = "lazygit",
   direction = "float",
   float_opts = {
@@ -8,16 +8,15 @@ local lazygit = Terminal:new({
   },
   -- function to run on opening the terminal
   on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.cmd "startinsert!"
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
   -- function to run on closing the terminal
-  on_close = function(term)
-    vim.cmd("startinsert!")
+  on_close = function()
+    vim.cmd "startinsert!"
   end,
-})
+}
 
 function LazygitToggle()
   lazygit:toggle()
 end
-
