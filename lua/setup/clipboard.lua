c2eee96ba5a309398b2ee27085f73b590b18bdfa
ngicks <yknt.bsl@gmail.com>
2025-03-14 01:19:58 +0900

    leave memo that you can't use unnamedplus without doing something

--- lua/setup/clipboard.lua
+++ lua/setup/clipboard.lua
@@ -1,4 +1,9 @@
 if vim.fn.has "wsl" == 1 then
+  -- on wsl, "unnamedplus" is wl-copy as far as I've observed.
+  -- and somehow, it fails to create wayland socket on the runtime dir.
+  -- That effectively disable us from using that.
+  -- xsel worked out excellently on my home and work environment.
+  -- I assume it works for most of you.
   vim.g.clipboard = {
     name = "WslClipboard",
     copy = {
