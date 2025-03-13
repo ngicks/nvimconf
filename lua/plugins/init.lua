7f75ca9381faf9958829a94232ff70b58389d781
ngicks <yknt.bsl@gmail.com>
2025-03-13 21:27:37 +0900

    add bqf and clean plugin config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,10 @@ return {
     opts = require "configs.conform",
   },
   {
+    "nvim-lua/plenary.nvim",
+  },
+  -- lsp
+  {
     "neovim/nvim-lspconfig",
     opts = require "configs.lspconfig",
   },
@@ -12,36 +16,53 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = require "configs.mason-lspconfig",
   },
+  -- telescope
   {
-    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension "fzf"
+    end,
+    keys = "<leader>",
   },
+  -- visual helper
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufWritePre",
+    event = { "BufReadPre", "BufNewFile" },
     opts = require "configs.treesitter",
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
-    event = "BufWritePre",
+    event = { "BufReadPre", "BufNewFile" },
     opts = require "configs.treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    event = "VeryLazy",
+    event = "BufReadPost *.md",
     opts = require "configs.render-md",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
   {
+    "kevinhwang91/nvim-bqf", -- preview for quick list items
+    event = "QuickFixCmdPre",
+    opts = require "configs.bqf",
+  },
+  -- util / command
+  {
     "akinsho/toggleterm.nvim",
+    event = "VeryLazy",
     version = "*",
     config = true,
   },
+  -- memo
   {
     "glidenote/memolist.vim",
-    event = "VeryLazy",
     config = function()
       require "configs.memolist"
     end,
+    cmd = { "MemoNew" },
   },
   {
     "delphinus/telescope-memo.nvim",
@@ -49,5 +70,6 @@ return {
       require "configs.telescope-memo"
     end,
     dependencies = { "glidenote/memolist.vim", "nvim-telescope/telescope.nvim" },
+    cmd = { "Telescope memo list", "Telescope memo live_grep" },
   },
 }
