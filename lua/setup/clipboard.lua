fbf3e1daea24be94d6bb7a1f529ec7c17af670d0
ngicks <yknt.bsl@gmail.com>
2025-03-17 02:12:19 +0900

    remove \r when copying text from windows
    Thought it would accidentally remove \r in binary file.
    However unfortunatelly it \r annoyed me more than hypothetical drawback.

--- lua/setup/clipboard.lua
+++ lua/setup/clipboard.lua
@@ -1,18 +1,20 @@
 if vim.fn.has "wsl" == 1 then
-  -- on wsl, "unnamedplus" is wl-copy as far as I've observed.
-  -- and somehow, it fails to create wayland socket on the runtime dir.
-  -- That effectively disable us from using that.
+  -- on wsl, "unnamedplus" is wl-copy as discribed in :h clipboard (wsl now has $WAYLAND_DISPLAY)
+  -- But somehow, wsl ubuntu 24.04.01 LTS fails to create wayland socket on the runtime dir.
+  -- That effectively disables us from using that.
   -- xsel worked out excellently on my home and work environment.
   -- I assume it works for most of you.
   vim.g.clipboard = {
-    name = "WslClipboard",
+    name = "xsel",
     copy = {
       ["+"] = "xsel -bi",
       ["*"] = "xsel -bi",
     },
     paste = {
-      ["+"] = "xsel -bo",
-      ["*"] = "xsel -bo",
+      ["+"] = function()
+        return vim.fn.systemlist "xsel -bo | sed 's/\\r$//'"
+      end,
+      ["*"] = "xsel -bo", -- is not actually used?
     },
     cache_enabled = 0,
   }
