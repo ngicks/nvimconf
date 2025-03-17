821e8b593cbdb2fb1559c8034e06033224bdb759
ngicks <yknt.bsl@gmail.com>
2025-03-17 23:58:50 +0900

    format markdown with prettier

--- lua/lazyconf/conform_opts.lua
+++ lua/lazyconf/conform_opts.lua
@@ -3,10 +3,11 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    markdown = { "prettier" },
   },
 
   format_on_save = {
-  -- These options will be passed to conform.format()
+    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
